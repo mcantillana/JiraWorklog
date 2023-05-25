65c03245bd55ffb5cea2b5dcf196bf09dc4064b0
Mike <myckel@okkami.com>
2023-05-25 15:48:36 -0400

    Added Google Sheets Integration for Data Storage: Now the Jira worklogs data can also be stored in a Google Sheet alongside CSV. Implemented using Google Sheets API

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
 # Jira Worklog Extractor
 
-Jira Worklog Extractor is a Python script that retrieves a user's worklog from Jira and exports it to a CSV file. You can filter the worklog by date range and project. Please note that this project is still a work in progress, and improvements are continuously being made.
+Jira Worklog Extractor is a Python script that retrieves a user's worklog from Jira and exports it to a CSV file and a Google Sheets document. You can filter the worklog by date range and project. Please note that this project is still a work in progress, and improvements are continuously being made.
 
 ## Installation
 
 To set up and run the Jira Worklog Extractor, follow these steps:
 
 1. Clone the repository or download the project files to your local machine.
-
 2. Make sure you have Python 3.6 or higher installed. You can check your Python version by running `python --version` or `python3 --version` in your terminal. And hey, if you're a Mac user, you already know that you need to use `python3` instead of `python`! 😉 (Just a friendly joke, we love all users equally)
-
 3. Install the required Python packages by running the following command in the terminal:
-
-~~~
-pip install -r requirements.txt
-~~~
-
-
-4. Create a `.env` file in the same directory as the script and fill in your Jira credentials, account ID to search, date range, output file, and project. Use the `.env.example` file as a reference.
-
+    ```
+    pip3 install -r requirements.txt
+    ```
+4. Create a `.env` file in the same directory as the script and fill in your Jira credentials, account ID to search, date range, output file, project, and Google service account JSON file path. Use the `.env.example` file as a reference.
 5. Run the script using the command:
-~~~
-python worklogs.py
+    ```
+    python3 worklogs.py
+    ```
 
-~~~
+## Google Sheets Integration
 
+The script can also create a Google Sheets document and populate it with the extracted worklog. To use this feature, follow these additional steps:
 
+1. Make sure the Google Sheets API is enabled for your Google account. [Here's how](https://developers.google.com/workspace/guides/create-project).
+2. Create a service account and download the JSON file. Place it in your project directory. [Here's how](https://developers.google.com/workspace/guides/create-credentials).
+3. Share the Google Sheets document with your service account. Use the `client_email` found in the JSON file.
+4. Add the path of your service account JSON file to the `.env` file (e.g., `GOOGLE_CREDENTIALS=google_credentials.json`).
 
 ## Contributing
 
--- worklogs.py
+++ worklogs.py
@@ -1,73 +1,94 @@
 import os
 import csv
+import pytz
+import gspread
 from atlassian import Jira
 from datetime import datetime
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
 
-# Cargar variables de entorno
+# Load environment variables
 load_dotenv()
 
-# Configuración
+# Configuration
 jira_url = os.getenv("JIRA_URL")
 jira_user = os.getenv("JIRA_USER")
 jira_password = os.getenv("JIRA_PASSWORD")
 jira_account_id_to_search = os.getenv("JIRA_ACCOUNT_ID_TO_SEARCH")
 start_date = os.getenv("START_DATE")
 end_date = os.getenv("END_DATE")
-output_file = os.getenv("OUTPUT_FILE")
 project = os.getenv("PROJECT")
+tz_from = pytz.timezone(os.getenv("TZ_FROM"))
+tz_to = pytz.timezone(os.getenv("TZ_TO"))
+output_file = os.getenv("OUTPUT_FILE")
+google_credentials = os.getenv("GOOGLE_CREDENTIALS") # path to your Google service account file
+google_account_email = os.getenv("GOOGLE_ACCOUNT_EMAIL")
 
-
-# Conexión a Jira
+# Connect to Jira
 jira = Jira(url=jira_url, username=jira_user, password=jira_password)
 
-# Convertir las fechas a objetos datetime
+# Convert dates to datetime objects
 start_date = datetime.strptime(start_date, "%Y-%m-%d")
 end_date = datetime.strptime(end_date, "%Y-%m-%d")
 
-# Función para filtrar worklogs por fechas
+# Google Sheets API credentials and connection
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(google_credentials, scope)
+gc = gspread.authorize(credentials)
+
+# Create a new Google Spreadsheet
+sheet = gc.create('Worklog')
+sheet.share(google_account_email, perm_type='user', role='writer')
+
+# Get the first sheet of the new Spreadsheet
+worksheet = sheet.sheet1
+
+# Function to filter worklogs by dates
 def filter_worklogs_by_date(worklogs, start_date, end_date):
     filtered_worklogs = []
     for worklog in worklogs:
-        worklog_date = datetime.strptime(worklog["started"][:10], "%Y-%m-%d")
-        if start_date <= worklog_date <= end_date:
+        dt = datetime.strptime(worklog["started"][:19], "%Y-%m-%dT%H:%M:%S")
+        dt = tz_from.localize(dt)
+        dt = dt.astimezone(tz_to)
+        dt = datetime.strptime(worklog["started"][:10], "%Y-%m-%d")
+        if start_date <= dt <= end_date:
+            worklog_date = dt.strftime('%Y-%m-%d')
             filtered_worklogs.append(worklog)
     return filtered_worklogs
 
-# Función para obtener el worklog de un usuario en Jira
+# Function to get a user's worklog in Jira
 def get_user_worklog(account_id, start_date, end_date):
-    # Buscar issues asignados al usuario
-
-    issues = jira.jql(f'project = "{project}" AND (assignee was {jira_account_id_to_search} OR assignee = {jira_account_id_to_search}) AND worklogDate >= startOfMonth()')
+    # Search issues assigned to the user
+    issues = jira.jql(f'project = "{project}" AND (worklogAuthor = {jira_account_id_to_search}) AND worklogDate >= startOfMonth() AND worklogDate <= endOfMonth()')
     # issues = jira.jql(f"assignee = {account_id}")
     total_issues = len(issues["issues"])
-    
+
     worklogs = []
     for index, issue in enumerate(issues["issues"]):
         issue_key = issue["key"]
         issue_data = jira.issue(issue_key, expand='changelog,renderedFields')
         issue_worklogs = issue_data["fields"]["worklog"]["worklogs"]
-        
-        # Mostrar el progreso de la búsqueda
-        print(f"Procesando issue {index + 1} de {total_issues}")
-        
-        # Filtrar worklogs por usuario y fechas
+
+        # Show search progress
+        print(f"Processing issue {index + 1} of {total_issues}")
+
+        # Filter worklogs by user and dates
         for worklog in issue_worklogs:
             if worklog["author"]["accountId"] == account_id:
                 worklogs.append(worklog)
-    
+
     filtered_worklogs = filter_worklogs_by_date(worklogs, start_date, end_date)
     return filtered_worklogs
 
-# Obtener el worklog del usuario
+# Get user's worklog
 user_worklog = get_user_worklog(jira_account_id_to_search, start_date, end_date)
 
-# Escribir el worklog en un archivo CSV
+# Write worklog to a CSV file
 with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['id', 'issueId', 'date', 'timeSpent', 'timeSpentHours']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    
+
     for worklog in user_worklog:
         writer.writerow({
             'id': worklog['id'],
@@ -77,4 +98,24 @@ with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
             'timeSpentHours': worklog['timeSpentSeconds']/3600
         })
 
-print(f"Worklog guardado en el archivo {output_file}")
+print(f"Worklog saved in the file {output_file}")
+
+# Prepare the data
+data = []
+fieldnames = ['id', 'issueId', 'date', 'timeSpent', 'timeSpentHours']
+data.append(fieldnames)
+
+for worklog in user_worklog:
+    row = [
+        worklog['id'],
+        worklog['issueId'],
+        worklog['started'][:10],
+        worklog['timeSpent'],
+        worklog['timeSpentSeconds']/3600
+    ]
+    data.append(row)
+
+# Update the sheet
+worksheet.update('A1', data)
+
+print(f"Worklog saved in Google Sheets file: {sheet.url}")
